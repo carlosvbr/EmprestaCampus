b9c7125a64273573e675475c7331ff1e8cef865f
Carlos Vinicius <viniciusborba2010@hotmail.com>
2026-08-30 18:26:10 -0300

    Implementação da Interface Web de Autenticação e Gestão de Credenciais

--- backend/usuarios/urls.py
+++ backend/usuarios/urls.py
@@ -1,10 +1,17 @@
 ﻿from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
-from .views import RegistroView
+from .views import RegistroView, login_view, home_view
 
 urlpatterns = [
+    # --- ROTAS DA API COM JWT
     path("registro/", RegistroView.as_view(), name="registro"),
-    path("login/", TokenObtainPairView.as_view(), name="login"),
+    path("login/", TokenObtainPairView.as_view(), name="login_api"), # Alterado levemente o 'name' para evitar conflito
     path("login/renovar/", TokenRefreshView.as_view(), name="login_renovar"),
+
+    # --- ROTAS DO FRONT-END HTML
+    path("entrar/", login_view, name="login_web"),
+
+    # --- ROTA DE DESTINO PÓS-LOGIN ---
+    path("home/", home_view, name="home"), # O name="home" conecta com o redirect('home')
 ]
--- backend/usuarios/views.py
+++ backend/usuarios/views.py
@@ -1,6 +1,9 @@
-﻿from rest_framework import generics, permissions
-
+﻿from django.http import HttpResponse
+from rest_framework import generics, permissions
 from .serializers import RegistroSerializer
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.contrib import messages
 
 
 class RegistroView(generics.CreateAPIView):
@@ -12,6 +15,32 @@ class RegistroView(generics.CreateAPIView):
     é salva assim: o RegistroSerializer.create() chama set_password,
     que aplica hash (PBKDF2, padrão do Django) antes de gravar no banco.
     """
-
     serializer_class = RegistroSerializer
     permission_classes = [permissions.AllowAny]
+
+def login_view(request):
+    """
+    Interface visual de login para validação da disciplina.
+    Integra a checagem segura de credenciais utilizando os models 
+    já estruturados na base de dados PostgreSQL.
+    """
+    if request.method == 'POST':
+        usuario_digitado = request.POST.get('username')
+        senha_digitada = request.POST.get('password')
+
+        user = authenticate(request, username=usuario_digitado, password=senha_digitada)
+
+        if user is not None:
+            login(request, user)
+            return redirect('home') 
+        else:
+            messages.error(request, 'Credenciais inválidas. Verifique seu acesso e tente novamente.')
+
+    return render(request, 'usuarios/login.html')
+
+def home_view(request):
+    """
+    View temporária apenas para confirmar que o login funcionou
+    e o redirecionamento foi feito com sucesso.
+    """
+    return HttpResponse("<h1>Login bem-sucedido! Bem-vindo ao EmprestaCampus.</h1><p>Esta é a tela inicial provisória.</p>")
